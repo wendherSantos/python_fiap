42e1aa8f0118f78142200f54c45cfcc9145fbef3
wendherSantos <wendher.santos@outlook.com>
2023-05-07 15:30:33 -0300

    atualizacao

--- python_fiap/capitulo5_manipula_arquivos/inventario.py
+++ python_fiap/capitulo5_manipula_arquivos/inventario.py
@@ -1,6 +1,7 @@
 from funcoes.funcoes_arquivos import *
 
 inventario = {}
+
 opcao = chamar_menu()
 while opcao>0 and opcao<4:
     if opcao == 1:
@@ -8,5 +9,7 @@ while opcao>0 and opcao<4:
     elif opcao == 2:
         persistir(inventario)
     elif opcao == 3:
-        print(exibir())
+        resultado = exibir()
+        for linha in resultado:
+            print(linha[2:-1])
     opcao = chamar_menu()
