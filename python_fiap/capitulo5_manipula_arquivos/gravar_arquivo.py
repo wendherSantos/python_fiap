fafc4ce292ad8f322e371617ff42627a43abf50d
wendherSantos <wendher.santos@outlook.com>
2023-05-07 12:02:28 -0300

    refactor_path

---
+++ python_fiap/capitulo5_manipula_arquivos/gravar_arquivo.py
@@ -0,0 +1,5 @@
+with open("teste.txt", "w") as arquivo:
+    arquivo.write("Nunca foi tão fácil criar um arquivo.")
+
+with open("teste.txt", "a") as arquivo:
+    arquivo.write("\nContinuação do texto.")
---
+++ python_fiap/capitulo5_manipula_arquivos/leitura_arquivo.py
@@ -0,0 +1,4 @@
+with open("teste.txt", "r") as arquivo:
+    conteudo = arquivo.readlines()
+print("Tipo de dado da variável", type(conteudo))
+print("Conteúdo do arquivo: ", conteudo)
